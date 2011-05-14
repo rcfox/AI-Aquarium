288e29b58bbab379ec8e1b3d50df84d7d6956d95
Ryan Fox <foxryan@gmail.com>
2011-05-13 23:22:15 -0400

    I don't know why I didn't put the function pointer typedef about the goal struct. Now that it is, I can use it in the struct.

--- goal.h
+++ goal.h
@@ -5,18 +5,18 @@
 
 struct entity;
 
+typedef bool (*goal_func)(struct entity* owner, TCOD_list_t params);
+
 typedef struct goal
 {
 	struct entity* owner;
-	bool (*pre_goal)(struct entity* owner, TCOD_list_t params);
-	bool (*goal_func)(struct entity* owner, TCOD_list_t params);
-	bool (*post_goal)(struct entity* owner, TCOD_list_t params);
+	goal_func pre_goal;
+	goal_func goal_func;
+	goal_func post_goal;
 	bool first_time;
 	TCOD_list_t parameters;
 	TCOD_list_t subgoals;
 } goal;
-
-typedef bool (*goal_func)(struct entity* owner, TCOD_list_t params);
 
 goal* goal_new(struct entity* owner, goal_func pre_goal, goal_func goal, goal_func post_goal, ...);
 void goal_delete(goal* g);
